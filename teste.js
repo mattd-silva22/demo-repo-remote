8419992689691372845ab031c179aaa395998470
matheus b <matheus2222br@gmail.com>
2021-02-08 12:51:57 -0300

    debug teste.js

--- teste.js
+++ teste.js
@@ -1,2 +1,6 @@
-var VarTeste = 1234
-alert(Varteste)
+function potencia() {
+	var x = document.getElementById('campo').value
+	var valor = x ** 2 
+	document.getElementById('campo-2').value = valor
+
+}
