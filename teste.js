fe790815a4bfdc14d2d6a1338d93fab1f736341d
matheus b <matheus2222br@gmail.com>
2021-02-08 13:18:35 -0300

    corrir bug , add funcao impar ou par

--- teste.js
+++ teste.js
@@ -4,3 +4,13 @@ function potencia() {
 	document.getElementById('campo-2').value = valor
 
 }
+
+function impar_par() {
+	var n = document.getElementById('campo-3').value
+	var resultado = n % 2 
+	if ( resultado == 1) {
+		document.getElementById('campo-4').value = "Impar"
+	} else {
+		document.getElementById('campo-4').value = "Par"
+	}
+}
